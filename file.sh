0e9593959b93fe1df4b32b71079a09879cc15367
Abdul Hadi Ahmadi <159695473+sarriry@users.noreply.github.com>
2024-03-18 18:25:15 +0430

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "enter a number"
+read num
+if [[ $num -eq 10]]
+then
+    echo "your number is less than 10"
+elif[[ $num -eq 9]]
+echo "the number is 9"
+elif[[ $num -eq 8]]
+echo"the number is 8"
+else
+echo "wrang number"
+fi
+
